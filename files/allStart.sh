8af2474b463dd202a8c496971aa44b8000216635
Michael Aldridge <aldridge.mac@gmail.com>
2015-05-28 02:04:40 -0500

    add start script

--- files/allStart.sh
+++ files/allStart.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
-byobu-tmux new-session -d -s minecraft
+tmux new-session -d -s minecraft
+
+# htop, useful to monitor host
+tmux new-window -t minecraft:0 -n 'htop' 'htop'
 
 # BUNGEECORD ROOT
 echo "Starting Core BungeeCord Services"
 # start the bungeecord core server
-byobu-tmux new-window -t minecraft:1 -n 'BungeeCord' 'cd bungeeCord; ./start.sh'
+tmux new-window -t minecraft:1 -n 'BungeeCord' 'cd bungeeCord; ./start.sh; bash'
 
 # lobby
 echo "Starting Core BungeeCord Lobby"
-byobu-tmux new-window -t minecraft:2 -n 'BungeeCordLobby' 'cd backend/lobby; ./start.sh'
+tmux new-window -t minecraft:2 -n 'BungeeCordLobby' 'cd backend/lobby; ./start.sh; bash'
 
 # BUNGEECORD BACKENDS
 # survival
 echo "Starting Survival"
-byobu-tmux new-window -t minecraft:3 -n 'Survival' 'cd backend/survival; ./start.sh'
+tmux new-window -t minecraft:3 -n 'Survival' 'cd backend/survival; ./start.sh; bash'
 
 # creative
 echo "Starting Creative"
-byobu-tmux new-window -t minecraft:4 -n 'Creative' 'cd backend/creative; ./start.sh'
+tmux new-window -t minecraft:4 -n 'Creative' 'cd backend/creative; ./start.sh; bash'
 
 # LAUNCH COMPLETE
 # attatch to the bungeecord session
 echo "Startup complete, connecting to BungeeCord Root"
-byobu-tmux select-window -t minecraft:1
-byobu-tmux -2 attach-session -t minecraft
+tmux select-window -t minecraft:1
+tmux -2 attach-session -t minecraft
 
